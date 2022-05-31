0d994665e16d4d8b115837c1f603b509cf8b4fc7
yukav91 <72371287+yukav91@users.noreply.github.com>
2022-05-31 22:45:18 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace test1
+{
+    class Class1
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            double a, b, c, d, x1, x2;
+            Console.WriteLine("Másodfokú egyenlet!\nKérlek, add meg az 'a' értékét:");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Kérlek, add meg a 'b' értékét:");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Kérlek, add meg a 'c' értékét:");
+            c = Convert.ToDouble(Console.ReadLine());
+            d = b * b - 4 * a * c;
+            x1 = (-b + Math.Sqrt(d)) / (2 * a);
+            x2 = (-b - Math.Sqrt(d)) / (2 * a);
+            Console.WriteLine("\nAz x1 értéke: " + x1 + "\nAz x2 értéke: " + x2);            
+        }
+    }
+}
